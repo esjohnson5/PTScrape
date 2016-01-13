0341b46550697f67a3eb4040f04e693266418af7
esjohnson5 <esjohnson5@wisc.edu>
2016-01-13 14:39:23 -0600

    updated fonts and makes things look better

--- scrape.js
+++ scrape.js
@@ -13,16 +13,29 @@ function scrape(){
 			var city_element = info.cells[1];
 			var city = city_element.innerHTML;
 			buildOutput(clinic_name,clinic_address,city,tbody.rows[0]);
+			var header = tbody.rows[0].getElementsByTagName('th');
+			header[0].innerHTML = " ";
+			header[1].innerHTML = " ";
+			header[2].innerHTML = " ";
+
+
 			//console.log(clinic_name);
 			//console.log(clinic_address);
 			//console.log(city);
 		}
-
 }
 
 function buildOutput(name, address, city, element){
 	var i = 0;
 	var cell = element.insertCell(i);
-	cell.innerHTML = "<div width='10%'><p>" + name + "<br>" + address + "<br>" + city + "</p></div>";
+	cell.innerHTML = "<div width='10%' class='outputtext'><p>" + name + "<br>" + address + "<br>" + city + "</p></div>";
 }
+
+function styles(){
+	var style = document.createElement("style");
+	var output = ".outputtext { font-family: Times; color: black; font-size: 11pt; padding: 2px 2px; text-align: center}";
+	style.innerHTML = output;
+	document.head.appendChild(style);
+}
+styles();
 scrape();
