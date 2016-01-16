a1ec33fcbcf3b95f7510a5adb698247ceda2cfcd
esjohnson5 <esjohnson5@wisc.edu>
2016-01-16 16:32:11 -0600

    now with geocoding!

---
+++ eventPage.js
@@ -0,0 +1,17 @@
+chrome.runtime.onMessage.addListener(
+	function(request,sender,sendResponse){
+		console.log(request.name);
+		sendResponse(makeReq(request));
+});
+
+function makeReq(addObj){
+	var city = addObj.city.replace(" ", "+");
+	var info = addObj.num + "+" + addObj.street +"," + "+" +  addObj.city + "," + "+" + addObj.state;
+	var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + info;
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET",url,false);
+	xhr.send();
+	var response = xhr.responseText;
+	var info = JSON.parse(response);
+	return info;
+}
--- scrape.js
+++ scrape.js
@@ -5,6 +5,12 @@ function scrape(){
 		var results = document.getElementsByClassName('searchresults')[0];
 		var tbody = results.getElementsByTagName('tbody')[0];
 		var row_no = tbody.rows.length;
+
+		var header = tbody.rows[0].getElementsByTagName('th');
+			header[0].innerHTML = " ";
+			header[1].innerHTML = " ";
+			header[2].innerHTML = " ";
+		
 		for (var i=3; i<row_no; i=i+2){
 			var info = tbody.rows[i];
 			var address_element = info.cells[0];
@@ -12,22 +18,51 @@ function scrape(){
 			var clinic_address = address_element.getElementsByTagName('p')[0].innerText;
 			var city_element = info.cells[1];
 			var city = city_element.innerHTML;
-			buildOutput(clinic_name,clinic_address,city,tbody.rows[0]);
-			var header = tbody.rows[0].getElementsByTagName('th');
-			header[0].innerHTML = " ";
-			header[1].innerHTML = " ";
-			header[2].innerHTML = " ";
+			var addressObj = buildAddObj(clinic_address,city);
+			
+			chrome.runtime.sendMessage(addressObj,function(response){	
+				console.log(response.results);
+				buildOutput(clinic_name,response.results,tbody.rows[0]);
+			});
 
-
+			
 			//console.log(clinic_name);
-			//console.log(clinic_address);
-			//console.log(city);
+			console.log(clinic_address);
+			console.log(city);
 		}
 }
 
-function buildOutput(name, address, city, element){
+function buildAddObj(add,c){
+	var tmp = add.split("\n");
+	var city = c.split(",")[0];
+	var state = c.split(",")[1];
+	var name,num;
+	var street = " ";
+	for(var i=0; i<tmp.length; i++){
+		if (isNaN(tmp[i].charAt(0))){
+			name = tmp[i];
+		}else{
+			var tmp2 = tmp[i].split(" ");
+			num = tmp2[0];
+			for(var j=1; j<tmp2.length; j++){
+				street = street + tmp2[j] + "+";
+			}
+		}		
+	}
+	var address = {
+		name: name,
+		num: num,
+		street: street, 
+		city: city,
+		state: state
+	};
+	return address;
+}
+
+function buildOutput(name, response, element){
 	var i = 0;
 	var cell = element.insertCell(i);
+	
 	cell.innerHTML = "<div width='10%' class='outputtext'><p>" + name + "<br>" + address + "<br>" + city + "</p></div>";
 }
 
